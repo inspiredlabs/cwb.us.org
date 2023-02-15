d7f281648e21b6fdfc76c4700e118196c2595471
Scott Phillips <alan.scott.phillips@protonmail.ch>
2023-02-15 18:58:48 +0100

    Added `~/uncover` a storytelling component demo.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,10 @@
 		<a class="" href="https://github.com/inspiredlabs/cwb.us.org/blob/master/README.md">Scope of work</a>
 	</li>
 	<!-- usage: https://hygraph.com/docs/guides/content/creating-content -->
-	<!--<li>
-			<a class="" href="https://app.hygraph.com/a546076c7f4c4bdaadebaab3525215aa/master/settings/project">Next-gen CMS</a>&nbsp;+&nbsp;<a class="" href="https://account.proton.me/login/clownswithoutborders@proton.me">mail</a>
+	<!---->
+		<li>
+			<a class="" href="https://account.proton.me/login/clownswithoutborders@proton.me">Email</a> authenticated
+			<a class="" href="https://app.hygraph.com/a546076c7f4c4bdaadebaab3525215aa/master/settings/project">next-gen CMS</a>
 		</li>
 		<li>
 			<a class="" href="https://www.cwb.us.org/posts.json">Automated SEO Suggestions</a>
@@ -19,10 +21,13 @@
 		<li>
 			<a class="" href="https://www.cwb.us.org/color">Color Suggestions</a>
 		</li>
-		<li>
-			<a class="" href="https://codepen.io/inspiredlabs/pen/ZEMEvzp">True Color Blog</a>
+		<li>Storytelling idioms:
+			<a class="" href="https://codepen.io/inspiredlabs/pen/ZEMEvzp">True Colors Blog</a>.
 		</li>
-	-->
+		<li>
+			Video reveal: how <a class="" href="https://cwb.us.org/uncover/">storytelling meets UX</a>
+		</li>
+
 </ul>
 
 <style>
